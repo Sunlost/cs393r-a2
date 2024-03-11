73a5044db90ad008dc3132ab7a55217fe006f97a
AbbhinavJayaraman <jayabbhi@gmail.com>
2024-03-11 18:16:46 -0500

    not much progress, may need a dynamic d_short and d_long that can scale based on range_max

--- src/particle_filter/particle_filter.cc
+++ src/particle_filter/particle_filter.cc
@@ -162,11 +162,11 @@ void ParticleFilter::Update(const vector<float>& ranges,
     float divisor = 0.05;
     // robustification will be on 14 - Expecting The Unexpected slide 28
 
-    float d_short = 1;
-    float d_short_divided = 20;
+    float d_short = 5;
+    float d_short_divided = 500;
 
-    float d_long = 1;
-    float d_long_divided = 20;
+    float d_long = 6;
+    float d_long_divided = 720;
     
     float delta = 0;
     for (size_t i = 0; i < scan_ptr.size(); ++i) {
